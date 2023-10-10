efbec926483fb3534ed18af2558b2efe8479c236
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-10-09 21:55:46 -0300

    Actualizar y Elmininar registros

--- router.js
+++ router.js
@@ -21,4 +21,31 @@ router.get("/create", (req, res) => {
 
 router.post("/save", crud.save);
 
+//Editar registros
+router.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  conexion.query("SELECT * FROM users WHERE id=?", [id], (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.render("edit.ejs", { user: results[0] });
+    }
+  });
+});
+
+router.post("/update", crud.update);
+
+//Eliminar registro
+
+router.get("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  conexion.query("DELETE FROM users WHERE id = ?", [id], (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
 module.exports = router;
